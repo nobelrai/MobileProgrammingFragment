9da62d8c825aa51038a63fb56c6abf10d34c8b03
nobelrai <mailtonobelrai@gmail.com>
2023-05-21 15:34:08 +0545

    initial commit

---
+++ app/src/main/java/com/example/unitfive/FragmentOneTwo.java
@@ -0,0 +1,13 @@
+package com.example.unitfive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FragmentOneTwo extends Activity {
+    @Override
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+
+        setContentView(R.layout.fragment_one_two);
+    }
+}
